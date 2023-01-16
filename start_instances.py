ff9a1a86a6d52e74ed69b000d81db9ca7ef479e3
Melanie Desaive <m.desaive@mailbox.org>
2023-01-16 18:00:06 +0000

    working with ids.

---
+++ start_instances.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+""" Start EC2 Instance. """
+
+# import sys
+# import pprint
+import argparse
+import textwrap
+import boto3
+from aws_keys import AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+
+def prepare_arguments():
+    """ Parse commandline arguments."""
+
+    parser = argparse.ArgumentParser(
+        prog='start_instances.py',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=textwrap.dedent('''\
+        Start a bunch of AWS EC2 instance..
+
+        Autor: Melanie Desaive <m.desaive@mailbox.org>
+        '''),
+        epilog=textwrap.dedent('''\
+        Examples:
+
+            ./start_instance.py --instance="i-xxxxx,i-xxxx"
+        '''))
+
+    parser.add_argument(
+        '-i', '--instances',
+        dest='instances',
+        help='Instances to start.',
+        required=True)
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    """ main :) """
+    args = prepare_arguments()
+    instance_list = args.instances.split(',')
+
+    ec2 = boto3.client(
+        'ec2',
+        region_name=AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    ec2.start_instances(InstanceIds=instance_list)
+
+
+if __name__ == "__main__":
+    main()
